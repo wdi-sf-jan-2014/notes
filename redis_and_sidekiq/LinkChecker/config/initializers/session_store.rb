651826d176b689268f964028fd37b86c8cb145d5
Tim Garcia <tim.m.garcia@gmail.com>
2014-02-12 07:12:47 -0800

    Initial checkin of LinkChecker.  This adds the basic functionality but does not contain the changes for redis and sidekiq yet.

---
+++ redis_and_sidekiq/LinkChecker/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LinkChecker::Application.config.session_store :cookie_store, key: '_LinkChecker_session'
---
+++ redis_and_sidekiq/LinkChecker/db/schema.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140212000839) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "links", force: true do |t|
+    t.string   "url"
+    t.integer  "http_response"
+    t.integer  "site_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "links", ["site_id"], name: "index_links_on_site_id", using: :btree
+
+  create_table "sites", force: true do |t|
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
